1f4365bda0c34fd4b0808038428e85cde72099fa
Chris Bush <christopher.bush@schneider-electric.com>
2016-04-26 14:17:30 -0400

    Updated for compatibility with Jenkins 2.0 workflow API

--- src/main/java/hudson/plugins/emma/CoverageReport.java
+++ src/main/java/hudson/plugins/emma/CoverageReport.java
@@ -1,6 +1,7 @@
 package hudson.plugins.emma;
 
 import hudson.model.AbstractBuild;
+import hudson.model.Run;
 import hudson.util.IOException2;
 import org.apache.commons.digester.Digester;
 import org.xml.sax.SAXException;
@@ -54,7 +55,7 @@ public final class CoverageReport extends AggregatedReport<CoverageReport/*dummy
     }
 
     @Override
-    public AbstractBuild<?,?> getBuild() {
+    public Run<?,?> getBuild() {
         return action.owner;
     }
 
